c01941efef956b6387c47b2d04a74c918523beff
EunJongSoo <eunjongsoo@gmail.com>
2017-03-22 22:50:15 +0900

    unit 헤더 선언

--- Classes/Unit.h
+++ Classes/Unit.h
@@ -38,26 +38,25 @@ public:
 	};
 	
 	struct Unit_info {
-		unit_type _unit_type;
-		int _hp;								// 체력
-		int _mp;								// 마력
-		int _shield;							// 쉴드
-		int _attack;							// 공격력
-		int _armor;								// 방어력
-	};
-
-	struct Upgrade_info {
-		upgrade_type _upgrade_type;
-		int _attack_upgrade_level;
-		int _defence_upgrade_level;
+		unit_type _unit_type;					// 유닛 종류
+		upgrade_type _upgrade_type;				// 업그레이드 종류
+		std::string _unit_name;					// 유닛 이름
+		unsigned int _killing_count;			// 적 유닛 죽인수
+		unsigned int _hp;						// 체력
+		unsigned int _mp;						// 마력
+		unsigned int _shield;					// 쉴드
+		unsigned int _attack;					// 공격력
+		unsigned int _armor;					// 방어력
+		unsigned int _attack_upgrade_level;		// 공격 업그레이드 수준
+		unsigned int _defence_upgrade_level;	// 방어 업그레이드 수준
 	};
 
 	struct production_info {
 		float _required_production_time;		// 필요 생산 시간
-		int _required_building;					// 필요 인구수
-		int _required_mineral;					// 필요 미네랄
-		int _required_gas;						// 필요 가스
-		int _required_supply;					// 필요 인구수
+		unsigned int _required_building;		// 필요 건물
+		unsigned int _required_mineral;			// 필요 미네랄
+		unsigned int _required_gas;				// 필요 가스
+		unsigned int _required_supply;			// 필요 인구수
 	};
 	
 	void attack_unit();
@@ -67,10 +66,7 @@ public:
 	void hold_unit();
 
 private:
-	std::string _unit_name;
-	
 	Unit_info _unit_info;
-	Upgrade_info _upgrade_info;
 	production_info _production_info;
 
 	float _move_speed;							// 이동속도
@@ -86,18 +82,16 @@ private:
 	float _move_y;								// 이동 목표_y
 	float _pos_x;								// 유닛 좌표_x
 	float _pos_y;								// 유닛 좌표_y
-	int _tile_x;								// 타일 위치_x
-	int _tile_y;								// 타일 위치_y
+	unsigned int _tile_x;						// 타일 위치_x
+	unsigned int _tile_y;						// 타일 위치_y
 	
-	float _production_time;						// 생산중인 시간
-	special_effects unit_effects;				// 현재 특수이팩트
+	float _production_time;						// 현재 생산 시간
+	special_effects _unit_effects;				// 현재 특수이팩트
 
 
 	// 상속받은 개체가 가져야한다.
-	Weapon* _unit_weapon;
+	UnitWeapon* _unit_weapon;
 	UnitAnimation* _unit_animation;
-
-
 };
 
 #endif
