a3ae56e1d842c6444867e74fe0a8cea6ab535266
EunJongSoo <eunjongsoo@gmail.com>
2017-04-06 15:17:57 +0900

    클래스 전방선언, 주석 추가, 플레이어 벡터 반환 함수 수정

--- Classes/UnitManager.cpp
+++ Classes/UnitManager.cpp
@@ -1,8 +1,10 @@
+#include <vector>
+#include "PlayerUnitManager.h"
 #include "UnitManager.h"
 #include "Unit.h"
+#include "Point.h"
 
-UnitManager::UnitManager() {
-}
+UnitManager::UnitManager() {}
 
 UnitManager::~UnitManager() {
 	int size = player_unit_manager_vector.size();
@@ -12,32 +14,49 @@ UnitManager::~UnitManager() {
 	player_unit_manager_vector.clear();
 }
 
+// 플레이어 추가
+// 함수 내용이 좀 지저분해서 개선이 필요하다.
 bool UnitManager::add_player() {
+	// 플레이어 색상은 플레이어의 숫자로 결정한다.
 	player_color color = (player_color)(player_unit_manager_vector.size());
 	
+	// 색상이 열거체 값이 max 인지 확인한다.
 	bool is_max = (color == player_color::max);
 
+	// max가 아니면 매니저를 추가한다.
 	if (!is_max) {
-		PlayerUnitManager* manager = new PlayerUnitManager(color);
+		PlayerUnitManager* manager = new PlayerUnitManager(color, player_type::user);
 		player_unit_manager_vector.push_back(manager);
 	}
 	
+	// 플레이어 추가 여부를 반환한다.
+	// max가 되면 생성을 실패하기 때문에 !를 붙여준다.
 	return !is_max;
+
+	// 필요기능
+	// 플레이어 숫자가 최대가 아니면 플레이어를 추가하고
+	// 최대면 플레이어를 추가 하지 않는다.
+	// 플레이어 추가 여부를 반환한다.
 }
 
-Unit* UnitManager::create_unit(const unit_type _type, const player_color _color, const float _pos_x, const float _pos_y) {
+// 유닛 생성하기
+Unit* UnitManager::create_unit(const unit_type _type, const player_color _color, const eun::Point& _point) {
+	// 임시 유닛 객체를 만든다.
+	Unit* unit = nullptr;
+	// 플레이어 매니저 벡터를 검사한다.
 	for (PlayerUnitManager* manager : player_unit_manager_vector) {
+		// 매니저의 색상을 확인해서 매개변수와 비교한다.
 		if (manager->get_color() == _color) {
-			return manager->create_unit(_type, _pos_x, _pos_y);
+			// 색상이 같으면 유닛을 생성하고 임시 객체에 반환받은 유닛 포인터를 저장한다.
+			unit = manager->create_unit(_type, _point);
 		}
 	}
-	return nullptr;
+	// 유닛 포인터를 반환한다.
+	return unit;
 }
-std::vector<Unit*>& UnitManager::get_unit_array() {
-	player_unit_vector.clear();
-	for (PlayerUnitManager* player : player_unit_manager_vector) {
-		player_unit_vector.insert(player_unit_vector.end(), player->get_unit_vector().begin(), player->get_unit_vector().end());
-	}
-	return player_unit_vector;
+
+// 유닛 벡터를 받환한다.
+std::vector<PlayerUnitManager*>& UnitManager::get_unit_vector() {
+	return player_unit_manager_vector;
 }
 
--- Classes/UnitManager.h
+++ Classes/UnitManager.h
@@ -1,24 +1,31 @@
 #ifndef UNIT_MANAGER_H_
 #define UNIT_MANAGER_H_
 
-#include <vector>
 #include "Header.h"
-#include "PlayerUnitManager.h"
+
+// class 穿号 識情
+class PlayerUnitManager;
+class Unit;
+
+// namespace class 穿号 識情
+namespace std {
+	template <typename T, typename A> class vector;
+}
+
+namespace eun {
+	class Point;
+}
 
 class UnitManager {
-private:
-	typedef PlayerUnitManager::player_color player_color;
 public:
 	UnitManager();
 	~UnitManager();
 
 	bool add_player();
-	//void delete_player();
-	Unit* create_unit(const unit_type _type, const player_color _color, const float _pos_x, const float _pos_y);
-	std::vector<Unit*>& get_unit_array();
+	Unit* create_unit(const unit_type _type, const player_color _color, const eun::Point& _point);
+	std::vector<PlayerUnitManager*>& get_unit_vector();
 
 private:
-	std::vector<Unit*> player_unit_vector;
 	std::vector<PlayerUnitManager*> player_unit_manager_vector;
 
 };
