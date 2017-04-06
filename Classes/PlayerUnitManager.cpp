caa424e81b75746d902c22479cbf5203fd5f3893
EunJongSoo <eunjongsoo@gmail.com>
2017-04-06 14:58:26 +0900

    주석 추가, 전방 선언 추가

--- Classes/PlayerUnitManager.cpp
+++ Classes/PlayerUnitManager.cpp
@@ -1,21 +1,42 @@
+#include <vector>
 #include "PlayerUnitManager.h"
 #include "Unit.h"
+#include "Point.h"
+
+// 매니저 생성시 색상과 컴퓨터인지 유저인지 매개변수를 받는다.
+PlayerUnitManager::PlayerUnitManager(player_color _color, player_type _type) : 
+	color(_color), 
+	type(_type)
+{}
 
 PlayerUnitManager::PlayerUnitManager() {}
+PlayerUnitManager::~PlayerUnitManager() {}
 
-PlayerUnitManager::PlayerUnitManager(player_color _color) : 
-	_color(_color) 
-{
-
-}
-
-PlayerUnitManager::~PlayerUnitManager()
-{
-}
-
-Unit* PlayerUnitManager::create_unit(unit_type _type, const float _pos_x, const float _pos_y) {
-	Unit* unit = Unit::create(_type, cocos2d::Vec2(_pos_x, _pos_y));
+// 유닛을 생선한다.
+Unit* PlayerUnitManager::create_unit(unit_type _type, const eun::Point& _point) {
+	// 유닛의 종류와 위치를 받아서 생성한다.
+	Unit* unit = Unit::create(_type, _point);
+	// 벡터에 저장한다.
 	unit_vector.push_back(unit);
+	// 저장한 유닛의 포인터를 반환한다.
 	return unit;
+}
+
+inline PlayerUnitManager::player_color PlayerUnitManager::get_color() const
+{
+	// 매니저가 담당하는 플레이어 색상을 반환한다.
+	 return color;
+}
+
+inline PlayerUnitManager::player_type PlayerUnitManager::get_player_type() const
+{
+	// 매니저가 담당하는 플레이어 타입을 반환한다.
+	return type;
+}
+
+inline std::vector<Unit*>& PlayerUnitManager::get_unit_vector()
+{
+	// 플레이어의 유닛 벡터를 반환한다.
+	return unit_vector;
 }
 
--- Classes/PlayerUnitManager.h
+++ Classes/PlayerUnitManager.h
@@ -1,37 +1,50 @@
 #ifndef PLAYER_UNIT_MANAGER_H_
 #define PLAYER_UNIT_MANAGER_H_
 
-#include <vector>
 #include "Header.h"
 
+// class 전방선언
 class Unit;
+
+// namespace class 전방선언
+namespace std {
+	template < typename T, typename A> class vector;
+}
+
+namespace eun {
+	class Point;
+}
+
 class PlayerUnitManager {
 public:
+	// 플레이어 색상 지정 열거체
 	enum player_color {
 		red, blue, max
 	};
+	
+	// 플레이어 타입 지정 열거체
 	enum player_type {
 		user, ai,
 	};
 
 public:
-	PlayerUnitManager(player_color _color);
+	PlayerUnitManager(player_color _color, player_type _type);
 	~PlayerUnitManager();
 
-	Unit* create_unit(unit_type _type, const float _pos_x, const float _pos_y);
+	Unit* create_unit(unit_type _type, const eun::Point& _point);
 
-	inline player_color get_color() const { return _color; }
-	inline std::vector<Unit*>& PlayerUnitManager::get_unit_vector() {
-		return unit_vector;
-	}
+	inline std::vector<Unit*>& PlayerUnitManager::get_unit_vector();
+	inline player_color get_color() const;
+	inline player_type get_player_type() const;
+	
 
 private:
 	PlayerUnitManager();
 	std::vector<Unit*> unit_vector;
 	
 	// 플레이어 구분
-	player_color _color;
-
+	player_color color;
+	player_type type;
 };
 
 #endif // !UNIT_MANAGER_H_
