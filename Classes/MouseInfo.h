38a8fcf0fdaff1e47480f28c1368f9328b59cbbf
EunJongSoo <eunjongsoo@gmail.com>
2017-04-06 16:33:12 +0900

    float 변수를 Point 클래스로 변경

--- Classes/MouseInfo.h
+++ Classes/MouseInfo.h
@@ -1,6 +1,8 @@
 #ifndef MOUSE_INFO_H_
 #define MOUSE_INFO_H_
 
+#include "Point.h"
+
 class MouseInfo {
 public:
 	enum mouse_state {
@@ -8,11 +10,7 @@ public:
 	};
 public:
 	MouseInfo() :
-		state(mouse_state::none),
-		start_pos_x(0.0f),
-		start_pos_y(0.0f),
-		end_pos_x(0.0f),
-		end_pos_y(0.0f)
+		state(mouse_state::none)
 	{}
 	~MouseInfo() {}
 
@@ -20,28 +18,22 @@ public:
 		state = (mouse_state)(_state);
 	}
 
-	inline void set_start_pos(float _x, float _y) {
-		start_pos_x = _x;
-		start_pos_y = _y;
+	inline void set_start_pos(const eun::Point& _start_pos) {
+		start_pos = _start_pos;
 	}
-	inline void set_end_pos(float _x, float _y) {
-		end_pos_x = _x;
-		end_pos_y = _y;
+	inline void set_end_pos(const eun::Point& _end_pos) {
+		end_pos = _end_pos;
 	}
 
 	// 埃立 立辟
 	inline mouse_state get_mouse_state() const { return state; }
-	inline float get_start_pos_x() const { return start_pos_x; }
-	inline float get_start_pos_y() const { return start_pos_y; }
-	inline float get_end_pos_x() const { return end_pos_x; }
-	inline float get_end_pos_y() const { return end_pos_y; }
+	inline eun::Point get_start_pos() const { return start_pos; }
+	inline eun::Point get_end_pos() const { return end_pos; }
 
-	// 流立 立辟
+private:
 	mouse_state state;
-	float start_pos_x;
-	float start_pos_y;
-	float end_pos_x;
-	float end_pos_y;
+	eun::Point start_pos;
+	eun::Point end_pos;
 };
 
 #endif // !MOUSE_INFO_H_
