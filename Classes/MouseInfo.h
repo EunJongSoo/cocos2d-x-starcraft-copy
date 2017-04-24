022f196536c811f53f2f9ecede89c4ee9809e2a1
EunJongSoo <eunjongsoo@gmail.com>
2017-04-24 15:50:57 +0900

    참조자 전달하기

--- Classes/MouseInfo.h
+++ Classes/MouseInfo.h
@@ -6,7 +6,7 @@
 class MouseInfo {
 public:
 	enum mouse_state {
-		none, L_down, L_dragging, L_drag, L_double, L_up, R_down, R_up
+		none, L_down, L_dragging, L_drag, L_double, L_up, R_down, R_up, move
 	};
 public:
 	MouseInfo() :
@@ -18,8 +18,8 @@ public:
 	inline void set_start_pos(const cocos2d::Vec2& _start_pos) { start_pos = _start_pos; }
 	inline void set_end_pos(const cocos2d::Vec2& _end_pos) { end_pos = _end_pos; }
 	inline mouse_state get_mouse_state() const { return state; }
-	inline cocos2d::Vec2 get_start_pos() const { return start_pos; }
-	inline cocos2d::Vec2 get_end_pos() const { return end_pos; }
+	inline const cocos2d::Vec2& get_start_pos() const { return start_pos; }
+	inline const cocos2d::Vec2& get_end_pos() const { return end_pos; }
 
 private:
 	mouse_state state;
