759c26c977e6d3b53e531dd4e35f02f84dd9daa3
EunJongSoo <eunjongsoo@gmail.com>
2018-03-21 00:09:40 +0900

    주석 추가

--- Classes/InputManager.cpp
+++ Classes/InputManager.cpp
@@ -1,5 +1,5 @@
 /****************************
-수정일 : 2017-02-19
+수정일 : 2018-03-20
 작성자 : 은종수
 파일명 : InputManager.cpp
 ****************************/
@@ -11,7 +11,13 @@
 #include "InputInfo.h"
 
 #include "Header.h"
+/*
+생성자에서 변수 초기화 해주고 키보드 마우스 매니저 클래스를 생성하고
+인풋 매니저 자식으로 추가한다.
 
+왜 자식으로 추가했는지 궁금해서 주석 처리하고 실행하니 동작을 안한다.
+왜그런지는 잘 모르겠다.
+*/
 InputManager::InputManager() : 
 	keyboard_manager(nullptr),
 	mouse_manager(nullptr)
@@ -26,12 +32,19 @@ InputManager::InputManager() :
 	this->addChild(keyboard_manager);
 	this->addChild(mouse_manager);
 }
-
+/*키보드, 마우스 매니저를 create로 생성하면 코코스에서 자동으로 안전해제를 한다.
+그래서 필요가 없는데 왜 등록을 했는지 모르겠다.*/
 InputManager::~InputManager() 
 {
 	SAFE_DELETE(keyboard_manager);
 	SAFE_DELETE(mouse_manager);
 }
+
+/*
+입력 받은걸 처리하는 프로세스인데 마우스쪽만 코딩을 해서 키보드쪽 처리 내용이 없다.
+마우스로 입력한 정보를 받아오는데 정보가 2개가 있다
+왜 그런지 기억이 안나는데.. 마우스 매니저 처리되는걸 확인해봐야 할거 같다.
+*/
 
 // 사용자에게서 입력받은 명령을 처리하는 방법
 void InputManager::input_process(const cocos2d::Vec2& _origin, InputInfo* const _input)
--- Classes/InputManager.h
+++ Classes/InputManager.h
@@ -1,5 +1,5 @@
 /****************************
-수정일 : 2017-02-19
+수정일 : 2018-03-20
 작성자 : 은종수
 파일명 : InputManager.h
 ****************************/
@@ -10,19 +10,27 @@
 #include "cocos2d.h"
 
 class InputInfo;
-class KeyboardInfo;
 class KeyboardManager;
+class KeyboardInfo;
 class MouseManager;
 class MouseInfo;
 
+/*인풋 매니저는 노드 클래스를 상속 받는다.
+코코스라는 그림판 위에서 입력을 받는걸 받아 오려면 노드 클래스가 필요하기 때문이다.
+
+이 인풋 매니저는 키보드 마우스에서 입력 받은 신호를 가지고 와서 
+InputInfo에 포장해 전달하는 역할을 한다.*/
 class InputManager : public cocos2d::Node {
 public:
 	InputManager();
 	~InputManager();
 
+	/*입력이 들어오는걸 처리하는 방법*/
 	void input_process(const cocos2d::Vec2& _origin, InputInfo* const _input);
 private:
+	/*키보드에서 들어오는 입력을 관리할 매니저*/
 	KeyboardManager* keyboard_manager;
+	/*마우스에서 들어오는 입력을 관리할 매니저*/
 	MouseManager* mouse_manager;
 };
 
