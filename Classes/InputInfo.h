ba43a0b7f8e694adcd8997297c2773799dcc49e8
EunJongSoo <eunjongsoo@gmail.com>
2018-03-21 17:31:36 +0900

    주석 수정

--- Classes/InputInfo.h
+++ Classes/InputInfo.h
@@ -1,5 +1,5 @@
 /****************************
-수정일 : 2017-02-19
+수정일 : 2018-03-21
 작성자 : 은종수
 파일명 : InputInfo.h
 ****************************/
@@ -8,30 +8,21 @@
 #define INPUT_INFO_H_
 
 #include "Header.h"
-
 #include "MouseInfo.h"
-//#include "key"
-
-//class MouseInfo;
-//class KeyboardInfo;
 
 class InputInfo {
 public:
 	InputInfo()	{}
 	~InputInfo() {
 		SAFE_DELETE(mouse_info);
-		SAFE_DELETE(normal_mouse_info);
 	}
 	inline MouseInfo * get_mouse_info() const { return mouse_info; }
-	inline MouseInfo * get_normal_mouse_info() const { return normal_mouse_info; }
-	inline void set_info(MouseInfo* _info, MouseInfo* _normal) {
+	inline void set_info(MouseInfo* _info) {
 		mouse_info = _info;
-		normal_mouse_info = _normal;
 	}
 
 private:
 	MouseInfo* mouse_info;
-	MouseInfo* normal_mouse_info;
 	//KeyboardInfo* keyboard_info;
 
 };
